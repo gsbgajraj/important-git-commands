469ea142a112a5c2009dac7e4508ed3be95ee961
Gajraj Singh Bhati <110357099+gsbgajraj@users.noreply.github.com>
2024-10-03 21:03:32 +0530

    Update git.js

--- git.js
+++ git.js
@@ -13,3 +13,6 @@ git config user.email "user2@example.com"
 #you can do it for particular repository temparary changes are also possible 
 git config user.name "Desired User"
 git config user.email "desired_user@example.com"
+
+new
+  git push origin <branch-name>: Push a specific branch to the remote.7. Undoing Changes:git reset <file>: Unstage a file.git reset --hard: Reset to the last commit and discard all changes.git checkout -- <file>: Discard changes in a file.git revert <commit-hash>: Revert a specific commit.8. Stashing Changes:git stash: Save uncommitted changes for later.git stash list: View all stashed changes.git stash apply: Apply stashed changes.git stash drop: Delete a stash.
